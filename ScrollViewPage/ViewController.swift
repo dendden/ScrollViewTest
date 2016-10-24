4300aae0b73e4143265eda1d9fbe1124c7ad6665
Denys Triasunov <dendden@users.noreply.github.com>
2016-10-24 22:29:10 +0300

    scroll view takes half the width of the screen in the middle

--- ScrollViewPage/ViewController.swift
+++ ScrollViewPage/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     
     var images = [UIImageView]()
-    var contentWidth: CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        scrollView.frame = CGRect(x: view.frame.size.width/4, y: 0.0, width: view.frame.size.width/2, height: view.frame.size.height)
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x)")
@@ -24,13 +28,14 @@ class ViewController: UIViewController {
             images.append(imageView)
             
             var newX: CGFloat = 0.0
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
-            contentWidth += view.frame.size.width
+            newX = scrollView.frame.midX + scrollView.frame.size.width * CGFloat(x)
+            contentWidth += scrollView.frame.size.width
             
             scrollView.addSubview(imageView)
-            imageView.frame = CGRect(x: newX-75, y: view.frame.size.height/2-75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX-150, y: view.frame.size.height/2-75, width: 150, height: 150)
         }
         
+        scrollView.clipsToBounds = false
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
